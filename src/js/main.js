9be7837c373c537e168c236ad93e00fb5a795841
chako <charlot.kolly@studentfr.ch>
2025-01-30 10:13:22 +0100

    partie facultative terminée

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 var button = document.querySelector(".button");
+var count = document.querySelector(".count");
 
 function isActive() {
   button.classList.toggle("is-active");
+  setTimeout(function () {
+    button.classList.remove("is-active");
+  }, 1000);
+
+  let currentCount = parseInt(count.textContent, 10);
+  count.textContent = currentCount + 1;
 }
+
 button.addEventListener("click", isActive);
